592679e73b9ff76dc9abf069b5ffa5b88dfa3cee
Scott Bergeson <bergeson00@gmail.com>
2022-05-25 14:22:08 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Scott Bergeson on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
